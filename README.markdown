de1d1405e287fcab2e5b606ce5038473f3f14da7
basyura <basyura@gmail.com>
2012-04-01 23:21:28 +0900

    first commit

---
+++ README.markdown
@@ -0,0 +1,3 @@
+this is a grease monkey script for twitter web page.
+
+this script add list's link to menu.
---
+++ add_lists.user.js
@@ -0,0 +1,44 @@
+// ==UserScript==
+// @name           add lists
+// @namespace      add_lists@basyura.org
+// @include        https://twitter.com/*
+// @include        http://twitter.com/*
+// @resource       jquery    http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js 
+// ==/UserScript==
+(function (w) {
+/*
+ *
+ */
+(function() { 
+  var head   = document.getElementsByTagName('head')[0]; 
+  var script = document.createElement('script');
+  script.type = 'text/javascript'; 
+  var jQuery   = GM_getResourceText('jquery');
+  script.innerHTML = jQuery;
+  head.appendChild(script); 
+  $ = w.$; 
+})();
+
+var screen_name = $('.dropdown-menu .fullname').text()
+
+var root = $('<li/>').attr('class', 'lists')
+           .appendTo('#global-actions');
+
+$('<a/>').attr('class', 'js-hover')
+  .attr('href', '/#!/' + screen_name + '/lists')
+  .text('L I S T')
+  .appendTo(root);
+
+/*
+GM_xmlhttpRequest({
+    method: 'GET',
+    url: 'https://api.twitter.com/1/lists.json?screen_name=' + screen_name,
+    onload: function(response) {
+      JSON.parse(response.responseText).lists.forEach(function(list) {
+          // list.name list.uri をつかってプルダウンメニューを作る
+        });
+    }
+});
+*/
+
+})(this.unsafeWindow || this);
